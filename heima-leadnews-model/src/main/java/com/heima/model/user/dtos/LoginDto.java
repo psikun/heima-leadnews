2662c4a75288ab3a09c2260eebdb52af0dc7cc98
psikun <psikun@foxmail.com>
2023-06-27 15:29:52 +0800

    feat: 新增swagger相关配置

--- heima-leadnews-model/src/main/java/com/heima/model/user/dtos/LoginDto.java
+++ heima-leadnews-model/src/main/java/com/heima/model/user/dtos/LoginDto.java
@@ -1,5 +1,6 @@
 package com.heima.model.user.dtos;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -11,15 +12,16 @@ import lombok.Data;
 @Data
 public class LoginDto {
 
-
     /**
      * 手机号
      */
+    @ApiModelProperty(value = "手机号",required = true)
     private String phone;
 
     /**
      * 密码
      */
+    @ApiModelProperty(value = "密码",required = true)
     private String password;
 
 }
--- heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/controller/v1/ApUserLoginController.java
+++ heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/controller/v1/ApUserLoginController.java
@@ -4,6 +4,7 @@ import com.heima.model.common.dtos.ResponseResult;
 import com.heima.model.user.dtos.LoginDto;
 import com.heima.model.user.pojos.ApUser;
 import com.heima.user.service.ApUserService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/login")
+@Api(value = "app端用户登录",tags = "app端用户登录")
 public class ApUserLoginController {
 
     @Autowired
